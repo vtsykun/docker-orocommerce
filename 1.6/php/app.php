07a828b917a165bf2c7842c7869e644993a659bc
Vladimir Tsykun <vtsykun@okvpn.org>
2019-12-10 23:35:01 +0300

    Added docker config for the oldest ORO versions

---
+++ 1.6/php/app.php
@@ -0,0 +1,27 @@
+<?php
+
+use Symfony\Component\HttpFoundation\Request;
+
+$loader = require __DIR__.'/../vendor/autoload.php';
+
+require_once __DIR__.'/../src/AppKernel.php';
+
+if (isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] === 'https') {
+    $_SERVER['HTTPS']='on';
+}
+
+$kernel = new AppKernel('prod', false);
+
+// For example 172.16.0.0/12 10.16.0.0/16
+$trustedProxies = getenv('TRUSTED_PROXIES');
+
+//$kernel = new AppCache($kernel);
+$request = Request::createFromGlobals();
+if ($trustedProxies) {
+    $trustedProxies = explode(' ', $trustedProxies);
+    Request::setTrustedProxies($trustedProxies);
+}
+
+$response = $kernel->handle($request);
+$response->send();
+$kernel->terminate($request, $response);
