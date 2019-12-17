a401e31a794ab193b20a7a1dc9a8d25ab832218a
Uladzimir Tsykun <vtsykun@okvpn.org>
2019-12-17 16:50:07 +0000

    Dump version 4.1.0-rc2

--- sync-docker.sh
+++ sync-docker.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 set -Eeox pipefail
 
-# Auto generated at 2019-12-12T00:14:32+00:00
+# Auto generated at 2019-12-17T16:50:07+00:00
 docker pull php:7.2-fpm-alpine;
 docker pull php:7.1-fpm-alpine;
 exitCode=0;
 
 
-docker build 1.6/ --tag='okvpn/orocommerce:1.6.39' --build-arg ORO_PLATFORM=1.6.39 && docker push 'okvpn/orocommerce:1.6.39' || exitCode=$(( exitCode | $?))
-docker build 1.6/ --tag='okvpn/orocommerce:1.6' --build-arg ORO_PLATFORM=1.6.39 && docker push 'okvpn/orocommerce:1.6' || exitCode=$(( exitCode | $?))
-docker rmi 'okvpn/orocommerce:1.6.39' || true
+docker build 4.1/ --tag='okvpn/orocommerce:4.1.0-rc2' --build-arg ORO_PLATFORM=4.1.0-rc2 && docker push 'okvpn/orocommerce:4.1.0-rc2' || exitCode=$(( exitCode | $?))
+docker build 4.1/ --tag='okvpn/orocommerce:4.1' --build-arg ORO_PLATFORM=4.1.0-rc2 && docker push 'okvpn/orocommerce:4.1' || exitCode=$(( exitCode | $?))
+docker rmi 'okvpn/orocommerce:4.1.0-rc2' || true
 
 exit $exitCode;
