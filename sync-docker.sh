80ee3f7b3cfd28c1bbd72ae99f2375166118c581
Uladzimir Tsykun <vtsykun@okvpn.org>
2020-01-14 16:53:39 +0000

    Dump version 4.1.0-rc4

--- sync-docker.sh
+++ sync-docker.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 set -Eeox pipefail
 
-# Auto generated at 2020-01-14T16:24:07+00:00
+# Auto generated at 2020-01-14T16:53:39+00:00
 docker pull php:7.2-fpm-alpine;
-docker pull php:7.1-fpm-alpine;
+docker pull php:7.3-fpm-alpine;
 exitCode=0;
 
 
