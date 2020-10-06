2a24cf0f2c81116cb823ef3750abfe592db56e4d
Uladzimir Tsykun <vtsykun@okvpn.org>
2020-10-06 12:26:36 +0000

    Dump version 4.2.0-beta

--- sync-docker.sh
+++ sync-docker.sh
@@ -1,15 +1,12 @@
 #!/usr/bin/env bash
 set -Eeox pipefail
 
-# Auto generated at 2020-10-06T11:10:04+00:00
+# Auto generated at 2020-10-06T12:26:36+00:00
 docker pull php:7.2-fpm-alpine;
 docker pull php:7.3-fpm-alpine;
 exitCode=0;
 
 
-docker build 4.1/ --tag='okvpn/orocommerce:4.1.9' --build-arg ORO_PLATFORM=4.1.9 --memory=4000mb && docker push 'okvpn/orocommerce:4.1.9' || exitCode=$(( exitCode | $?))
-docker build 4.1/ --tag='okvpn/orocommerce:4.1' --build-arg ORO_PLATFORM=4.1.9 --memory=4000mb && docker push 'okvpn/orocommerce:4.1' || exitCode=$(( exitCode | $?))
-docker rmi 'okvpn/orocommerce:4.1.9' || true
 docker build 4.1/ --tag='okvpn/orocommerce:4.2.0-beta' --build-arg ORO_PLATFORM=4.2.0-beta --memory=4000mb && docker push 'okvpn/orocommerce:4.2.0-beta' || exitCode=$(( exitCode | $?))
 docker build 4.1/ --tag='okvpn/orocommerce:4.2' --build-arg ORO_PLATFORM=4.2.0-beta --memory=4000mb && docker push 'okvpn/orocommerce:4.2' || exitCode=$(( exitCode | $?))
 docker rmi 'okvpn/orocommerce:4.2.0-beta' || true
