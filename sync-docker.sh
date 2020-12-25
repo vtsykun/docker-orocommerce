d27fbb7a07eedd39fbc75cb360ea960bcc95b21a
Uladzimir Tsykun <vtsykun@okvpn.org>
2020-12-25 22:50:12 +0000

    Dump version 4.1.10, 4.1.11, 4.2.0-rc

--- sync-docker.sh
+++ sync-docker.sh
@@ -1,15 +1,17 @@
 #!/usr/bin/env bash
 set -Eeox pipefail
 
-# Auto generated at 2020-12-22T22:50:10+00:00
+# Auto generated at 2020-12-25T22:50:12+00:00
 docker pull php:7.2-fpm-alpine;
 docker pull php:7.3-fpm-alpine;
 exitCode=0;
 
 
 docker build 4.1/ --tag='okvpn/orocommerce:4.1.10' --build-arg ORO_PLATFORM=4.1.10 --memory=4000mb && docker push 'okvpn/orocommerce:4.1.10' || exitCode=$(( exitCode | $?))
-docker build 4.1/ --tag='okvpn/orocommerce:4.1' --build-arg ORO_PLATFORM=4.1.10 --memory=4000mb && docker push 'okvpn/orocommerce:4.1' || exitCode=$(( exitCode | $?))
+docker build 4.1/ --tag='okvpn/orocommerce:4.1.11' --build-arg ORO_PLATFORM=4.1.11 --memory=4000mb && docker push 'okvpn/orocommerce:4.1.11' || exitCode=$(( exitCode | $?))
+docker build 4.1/ --tag='okvpn/orocommerce:4.1' --build-arg ORO_PLATFORM=4.1.11 --memory=4000mb && docker push 'okvpn/orocommerce:4.1' || exitCode=$(( exitCode | $?))
 docker rmi 'okvpn/orocommerce:4.1.10' || true
+docker rmi 'okvpn/orocommerce:4.1.11' || true
 docker build 4.2/ --tag='okvpn/orocommerce:4.2.0-rc' --build-arg ORO_PLATFORM=4.2.0-rc --memory=4000mb && docker push 'okvpn/orocommerce:4.2.0-rc' || exitCode=$(( exitCode | $?))
 docker build 4.2/ --tag='okvpn/orocommerce:4.2' --build-arg ORO_PLATFORM=4.2.0-rc --memory=4000mb && docker push 'okvpn/orocommerce:4.2' || exitCode=$(( exitCode | $?))
 docker rmi 'okvpn/orocommerce:4.2.0-rc' || true
